826156d04ae1ffa7cc1a6b51117634363a47295e
intal-error <intalmarclawrence101604@gmail.com>
2026-08-25 00:08:33 +0800

    Remove BTVTEd program, keep specializations as standalone entries

--- frontend/src/constants/courses.js
+++ frontend/src/constants/courses.js
@@ -1,11 +1,10 @@
 export const COURSES = [
   "BIT",
-  "BTVTEd",
-  "BTVTEd CT",
-  "BTVTEd MT",
-  "BTVTEd FSM",
-  "BTVTEd AT",
-  "BTVTEd CPT",
-  "BTVTEd ELT",
-  "BTVTEd ELX",
+  "CT",
+  "MT",
+  "FSM",
+  "AT",
+  "CPT",
+  "ELT",
+  "ELX",
 ];
